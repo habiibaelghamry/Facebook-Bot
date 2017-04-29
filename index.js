c77fa37316c8fe6df8de2a1363b743d72f8762b1
habiibaelghamry <habiiba.elghamry@gmail.com>
2017-04-29 15:09:06 +0200

    Connect to Fasa7ny

--- index.js
+++ index.js
@@ -467,7 +467,7 @@ function getDailyEvents(sender, events, eventoccs, name) {
 			"image_url": "https://media-cdn.tripadvisor.com/media/photo-s/07/3f/a2/83/icon.jpg",
 			"buttons" : [{
                         "type": "web_url",
-                        "url": "http://54.187.92.64:8000/#!/viewOccurences/"+event[l]._id,
+                        "url": "https://54.187.92.64:8000/#!/viewOccurences/"+event[l]._id,
                         "title": "View Occurrences"
                     }]
 		})
