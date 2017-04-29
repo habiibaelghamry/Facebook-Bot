d1fc8b9ede0230489d0b88397aea7d2ee2860e04
habiibaelghamry <habiiba.elghamry@gmail.com>
2017-04-29 21:51:18 +0200

    Phone numbers

--- index.js
+++ index.js
@@ -135,6 +135,7 @@ app.post('/webhook/', function (req, res) {
 
             text = event.message.text
             if (text === 'Hi') {
+            	console.log("Ana fl Hiiiii");
                 sendGenericMessage(sender)
                 continue
             }
